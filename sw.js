61e4ca7186ed6467e8ada36b85ad6587d9eabf4f
Ivediouf84 <halilou170684@gmail.com>
2026-08-03 04:05:01 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
-const CACHE_NAME = 'novadoc-v2026-08-01-v48';
+const CACHE_NAME = 'novadoc-v2026-08-01-v49';
 const ASSETS = [
   './',
   './index.html',
-  './styles.css?v=20260801_v48',
+  './styles.css?v=20260801_v49',
   './novadoc-logo-n.png?v=12',
   './manifest.json',
   './icon-192.png',
